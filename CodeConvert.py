dad5847d0cb595aed9128cbdece8ceab198c855b
Brightcells <brightcells@gmail.com>
2015-11-02 16:29:53 +0800

    Fix Bug: https://github.com/Brightcells/CodeConvert/issues/3

--- CodeConvert.py
+++ CodeConvert.py
@@ -37,6 +37,7 @@ TIPS_UTF8 = {
     'unicode_without_u': u">>> 无 u 的 unicode 编码: obj.decode('raw_unicode_escape').encode('utf8')",
 }
 
+
 TIPS_UNICODE = {
     'u_contain_utf8': u">>> u 内含 utf8 编码: obj.decode('utf8')",
     'u_contain_gbk': u">>> u 内含 gbk 编码: obj.encode('raw_unicode_escape').decode('gbk') or obj.encode('latin1').decode('gbk')",
@@ -46,19 +47,23 @@ TIPS_UNICODE = {
     'unicode_without_u': u">>> 无 u 的 unicode 编码: obj.decode('raw_unicode_escape')",
 }
 
+
 TIPS_ESCAPE = {
     'double_backslash_str': u">>> 双反斜杠字符: obj.decode('string_escape')",
     'double_backslash_unicode': u">>> 双反斜杠 Unicode 字符: obj.decode('raw_unicode_escape')",
 }
 
 
-def print_tip_utf8(k): print TIPS_UTF8[k]
+def print_tip_utf8(k, debug=False):
+    print TIPS_UTF8[k] if debug else ''
 
 
-def print_tip_unicode(k): print TIPS_UNICODE[k]
+def print_tip_unicode(k, debug=False):
+    print TIPS_UNICODE[k] if debug else ''
 
 
-def print_tip_escape(k): print TIPS_ESCAPE[k]
+def print_tip_escape(k, debug=False):
+    print TIPS_ESCAPE[k] if debug else ''
 
 
 def exec_strip(repr_kw):
@@ -76,30 +81,41 @@ def convert_2_utf8_basestring(kw, debug=False):
         if striped_kw.startswith('\\x'):  # 处理 u 内含 gbk、utf8 编码
             try:  # 处理 u 内含 utf8 编码
                 kw.encode('raw_unicode_escape').decode('utf8')  # 如果是 u 内含 gbk 编码，会出错进入 except
-                if debug: print_tip_utf8('u_contain_utf8')
-                return kw.encode('raw_unicode_escape')
-            except UnicodeDecodeError:  # 处理 u 内含 gbk 编码
-                if debug: print_tip_utf8('u_contain_gbk')
-                return kw.encode('raw_unicode_escape').decode('gbk').encode('utf8')
+                ret = kw.encode('raw_unicode_escape')
+                print_tip_utf8('u_contain_utf8', debug)
+            except (UnicodeEncodeError, UnicodeDecodeError):  # 处理 u 内含 gbk 编码
+                try:
+                    ret = kw.encode('raw_unicode_escape').decode('gbk').encode('utf8')
+                    print_tip_utf8('u_contain_gbk', debug)
+                except (UnicodeEncodeError, UnicodeDecodeError):  # 处理 unicode 编码
+                    ret = kw.encode('utf8')
+                    print_tip_utf8('unicode', debug)
         elif isinstance(kw, unicode):  # 处理 unicode 编码
-            if debug: print_tip_utf8('unicode')
-            return kw.encode('utf8')
+            ret = kw.encode('utf8')
+            print_tip_utf8('unicode', debug)
+        else:
+            ret = kw
     else:
         striped_kw = exec_strip(repr_kw)
         if striped_kw.startswith('\\x'):  # 处理 gbk、utf8 编码
             try:  # 处理 utf8 编码
                 kw.decode('utf8')
-                if debug: print_tip_utf8('utf8')
-                return kw
+                ret = kw
+                print_tip_utf8('utf8', debug)
             except UnicodeDecodeError:  # 处理 gbk 编码
-                if debug: print_tip_utf8('gbk')
-                return kw.decode('gbk').encode('utf8')
+                try:
+                    ret = kw.decode('gbk').encode('utf8')
+                    print_tip_utf8('gbk', debug)
+                except UnicodeDecodeError:  # 处理无 u 的 unicode 编码
+                    ret = kw.decode('raw_unicode_escape').encode('utf8')
+                    print_tip_utf8('unicode_without_u', debug)
         elif striped_kw.startswith('\\\\u'):  # 处理无 u 的 unicode 编码
-            if debug: print_tip_utf8('unicode_without_u')
-            return kw.decode('raw_unicode_escape').encode('utf8')
+            ret = kw.decode('raw_unicode_escape').encode('utf8')
+            print_tip_utf8('unicode_without_u', debug)
         else:
-            if debug: print_tip_utf8('utf8')
-            return kw
+            ret = kw
+            print_tip_utf8('utf8', debug)
+    return ret
 
 
 def convert_2_unicode_basestring(kw, debug=False):
@@ -109,47 +125,58 @@ def convert_2_unicode_basestring(kw, debug=False):
         if striped_kw.startswith('\\x'):  # 处理 u 内含 gbk、utf8 编码
             try:  # 处理 u 内含 utf8 编码
                 kw.encode('latin1').decode('utf8')
-                if debug: print_tip_unicode('u_contain_utf8')
-                return kw.encode('latin1').decode('utf8')
-            except UnicodeDecodeError:  # 处理 u 内含 gbk 编码
-                if debug: print_tip_unicode('u_contain_gbk')
-                return kw.encode('latin1').decode('gbk')
+                ret = kw.encode('latin1').decode('utf8')
+                print_tip_unicode('u_contain_utf8', debug)
+            except (UnicodeEncodeError, UnicodeDecodeError):  # 处理 u 内含 gbk 编码
+                try:
+                    ret = kw.encode('latin1').decode('gbk')
+                    print_tip_unicode('u_contain_gbk', debug)
+                except (UnicodeEncodeError, UnicodeDecodeError):  # 处理 unicode 编码
+                    ret = kw
+                    print_tip_unicode('unicode', debug)
         elif isinstance(kw, unicode):  # 处理 unicode 编码
-            if debug: print_tip_unicode('unicode')
-            return kw
+            ret = kw
+            print_tip_unicode('unicode', debug)
+        else:
+            ret = kw
     else:
         striped_kw = exec_strip(repr_kw)
         if striped_kw.startswith('\\x'):  # 处理 gbk、utf8 编码
             try:  # 处理 utf8 编码
-                if debug: print_tip_unicode('utf8')
-                return kw.decode('utf8')
+                ret = kw.decode('utf8')
+                print_tip_unicode('utf8', debug)
             except UnicodeDecodeError:  # 处理 gbk 编码
-                if debug: print_tip_unicode('gbk')
-                return kw.decode('gbk')
+                try:
+                    ret = kw.decode('gbk')
+                    print_tip_unicode('gbk', debug)
+                except UnicodeDecodeError:  # 处理无 u 的 unicode 编码
+                    ret = kw.decode('raw_unicode_escape')
+                    print_tip_unicode('unicode_without_u', debug)
         elif striped_kw.startswith('\\\\u'):  # 处理无 u 的 unicode 编码
-            if debug: print_tip_unicode('unicode_without_u')
-            return kw.decode('raw_unicode_escape')
+            ret = kw.decode('raw_unicode_escape')
+            print_tip_unicode('unicode_without_u', debug)
         else:
-            if debug: print_tip_unicode('utf8')
-            return kw.decode('utf8')
+            ret = kw.decode('utf8')
+            print_tip_unicode('utf8', debug)
+    return ret
 
 
 def kw_escape(kw, debug=False):
-    repr_kw = repr(kw)
+    ret, repr_kw = kw, repr(kw)
     if repr_kw.startswith('u'):
         striped_kw = exec_strip(repr_kw[1:])
         if striped_kw.startswith('\\\\x'):
-            if debug: print_tip_escape('double_backslash_str')
-            return kw.decode('string_escape')
+            ret = kw.decode('string_escape')
+            print_tip_escape('double_backslash_str', debug)
         elif striped_kw.startswith('\\\\u'):
-            if debug: print_tip_escape('double_backslash_unicode')
-            return kw.decode('raw_unicode_escape')
+            ret = kw.decode('raw_unicode_escape')
+            print_tip_escape('double_backslash_unicode', debug)
     else:
         striped_kw = exec_strip(repr_kw)
         if striped_kw.startswith('\\\\x'):
-            if debug: print_tip_escape('double_backslash_str')
-            return kw.decode('string_escape')
-    return kw
+            ret = kw.decode('string_escape')
+            print_tip_escape('double_backslash_str', debug)
+    return ret
 
 
 def convert_2_utf8(kw):
@@ -163,8 +190,7 @@ def convert_2_utf8(kw):
         return tuple([convert_2_utf8(k) for k in kw])
     elif isinstance(kw, set):
         return set([convert_2_utf8(k) for k in kw])
-    else:
-        return kw
+    return kw
 
 
 def convert_2_unicode(kw):
@@ -178,8 +204,7 @@ def convert_2_unicode(kw):
         return tuple([convert_2_unicode(k) for k in kw])
     elif isinstance(kw, set):
         return set([convert_2_unicode(k) for k in kw])
-    else:
-        return kw
+    return kw
 
 
 class CodeConvert:
@@ -256,6 +281,14 @@ def _Convert2Utf8():
     # unicode 编码英文
     utf8_test(u'The last question')
 
+    # 全角
+    utf8_test('￥全角￥')
+    utf8_test(u'￥全角￥')
+
+    # 半角
+    utf8_test('¥半角¥')
+    utf8_test(u'¥半角¥')
+
 
 def _Convert2Unicode():
 
@@ -296,6 +329,14 @@ def _Convert2Unicode():
 
     # unicode 编码英文
     unicode_test(u'The last question')
+
+    # 全角
+    unicode_test('￥全角￥')
+    unicode_test(u'￥全角￥')
+
+    # 半角
+    unicode_test('¥半角¥')
+    unicode_test(u'¥半角¥')
 
 
 def main():
